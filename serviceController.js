cdf24ada22e01b67ffc834150371a77b1d0b5bb9
sainsaji <95070425+sainsaji@users.noreply.github.com>
2022-02-04 13:42:35 +0530

    Commit 1

---
+++ serviceController.js
@@ -0,0 +1,77 @@
+var serviceAppModule = angular.module("serviceApp", []);    
+ serviceAppModule.controller("serviceCtrl", function ($scope,$http) {    
+   $http.get('service.json')    
+   .success(function(data){    
+     $scope.services = data;    
+   })    
+   .error(function(data,status){    
+     console.error('Fail to load data', status, data);    
+     $scope.services = { };     
+   });    
+ }); 
+
+ serviceAppModule.filter("toUpperCase", function () {
+  return function (input)
+  {
+      var output = "";       
+      output = input.toUpperCase();
+      return output;
+  }
+});
+
+serviceAppModule.filter('feeGreaterThan', function () {
+ 
+  return function (input, fee) {
+      var output = [];
+      if (isNaN(fee)) {
+
+          output = input;
+      }
+      else {
+          angular.forEach(input, function (item) {
+              if (item.fee > fee) {
+                  output.push(item)
+              }
+          });
+      }
+      return output;
+  }
+})
+
+serviceAppModule.filter('onlytwoWheeler', function () {
+ 
+  return function (input, sclass) {
+      var output = [];
+      if (isNaN(sclass)) {
+
+          output = input;
+      }
+      else {
+          angular.forEach(input, function (item) {
+              if (item.sclass = "2 Wheeler") {
+                  output.push(item)
+              }
+          });
+      }
+      return output;
+  }
+})
+
+serviceAppModule.filter('onlyfourWheeler', function () {
+ 
+  return function (input, sclass) {
+      var output = [];
+      if (isNaN(sclass)) {
+
+          output = input;
+      }
+      else {
+          angular.forEach(input, function (item) {
+              if (item.sclass = "4 Wheeler") {
+                  output.push(item)
+              }
+          });
+      }
+      return output;
+  }
+})
